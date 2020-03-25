79de5810a7050f93bd142990a6866dad4a912297
Kevin Park <keunsuk413@gmail.com>
2020-03-24 20:17:45 -0500

    completed refactoring

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -1,46 +1,23 @@
-const { expect } = require('chai');
-const knex = require('knex');
-const app = require('../src/app');
-const { makeArticlesArray } = require('./articles.fixtures');
+const knex = require('knex')
+const app = require('../src/app')
+const { makeArticlesArray, makeMaliciousArticle } = require('./articles.fixtures')
 
-describe.only('Articles Endpoints', function() {
-  let db;
+describe('Articles Endpoints', function() {
+  let db
 
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL
-    });
-    app.set('db', db);
-  });
+      connection: process.env.TEST_DB_URL,
+    })
+    app.set('db', db)
+  })
 
-  after('disconnect from db', () => db.destroy());
-  before('clean the table', () => db('blogful_articles').truncate());
-  afterEach('cleanup', () => db('blogful_articles').truncate()); // avoid dup for next test
+  after('disconnect from db', () => db.destroy())
 
-//   context('Given there are articles in the database', () => {
-//     const testArticles = makeArticlesArray();
+  before('clean the table', () => db('blogful_articles').truncate())
 
-//     beforeEach('insert articles', () => {
-//       return db.into('blogful_articles').insert(testArticles);
-//     });
-
-//     it('GET /articles responds with 200 and all of the articles', () => {
-//       return supertest(app)
-//         .get('/articles')
-//         .expect(200)
-//         .expect(200, testArticles);
-//     });
-
-//     it('GET /articles/:article_id responds with 200 and the specified article', () => {
-//       const articleId = 2;
-//       const expectedArticle = testArticles[articleId - 1];
-//       return supertest(app)
-//         .get(`/articles/${articleId}`)
-//         .expect(200, expectedArticle);
-//     });
-//   });
-// });
+  afterEach('cleanup',() => db('blogful_articles').truncate())
 
   describe(`GET /articles`, () => {
     context(`Given no articles`, () => {
@@ -50,7 +27,7 @@ describe.only('Articles Endpoints', function() {
           .expect(200, [])
       })
     })
-    
+
     context('Given there are articles in the database', () => {
       const testArticles = makeArticlesArray()
 
@@ -66,6 +43,26 @@ describe.only('Articles Endpoints', function() {
           .expect(200, testArticles)
       })
     })
+
+    context(`Given an XSS attack article`, () => {
+      const { maliciousArticle, expectedArticle } = makeMaliciousArticle()
+
+      beforeEach('insert malicious article', () => {
+        return db
+          .into('blogful_articles')
+          .insert([ maliciousArticle ])
+      })
+
+      it('removes XSS attack content', () => {
+        return supertest(app)
+          .get(`/articles`)
+          .expect(200)
+          .expect(res => {
+            expect(res.body[0].title).to.eql(expectedArticle.title)
+            expect(res.body[0].content).to.eql(expectedArticle.content)
+          })
+      })
+    })
   })
 
   describe(`GET /articles/:article_id`, () => {
@@ -77,7 +74,7 @@ describe.only('Articles Endpoints', function() {
           .expect(404, { error: { message: `Article doesn't exist` } })
       })
     })
-    
+
     context('Given there are articles in the database', () => {
       const testArticles = makeArticlesArray()
 
@@ -94,6 +91,89 @@ describe.only('Articles Endpoints', function() {
           .get(`/articles/${articleId}`)
           .expect(200, expectedArticle)
       })
+    })
+
+    context(`Given an XSS attack article`, () => {
+      const { maliciousArticle, expectedArticle } = makeMaliciousArticle()
+
+      beforeEach('insert malicious article', () => {
+        return db
+          .into('blogful_articles')
+          .insert([ maliciousArticle ])
+      })
+
+      it('removes XSS attack content', () => {
+        return supertest(app)
+          .get(`/articles/${maliciousArticle.id}`)
+          .expect(200)
+          .expect(res => {
+            expect(res.body.title).to.eql(expectedArticle.title)
+            expect(res.body.content).to.eql(expectedArticle.content)
+          })
+      })
+    })
+  })
+
+  describe(`POST /articles`, () => {
+    it(`creates an article, responding with 201 and the new article`, function() {
+      this.retries(3)
+      const newArticle = {
+        title: 'Test new article',
+        style: 'Listicle',
+        content: 'Test new article content...'
+      }
+      return supertest(app)
+        .post('/articles')
+        .send(newArticle)
+        .expect(201)
+        .expect(res => {
+          expect(res.body.title).to.eql(newArticle.title)
+          expect(res.body.style).to.eql(newArticle.style)
+          expect(res.body.content).to.eql(newArticle.content)
+          expect(res.body).to.have.property('id')
+          expect(res.headers.location).to.eql(`/articles/${res.body.id}`)
+          const expected = new Date().toLocaleString()
+          const actual = new Date(res.body.date_published).toLocaleString()
+          expect(actual).to.eql(expected)
+        })
+        .then(res =>
+          supertest(app)
+            .get(`/articles/${res.body.id}`)
+            .expect(res.body)
+        )
+    })
+
+    const requiredFields = ['title', 'style', 'content']
+
+    requiredFields.forEach(field => {
+      const newArticle = {
+        title: 'Test new article',
+        style: 'Listicle',
+        content: 'Test new article content...'
+      }
+
+      it(`responds with 400 and an error message when the '${field}' is missing`, () => {
+        delete newArticle[field]
+
+        return supertest(app)
+          .post('/articles')
+          .send(newArticle)
+          .expect(400, {
+            error: { message: `Missing '${field}' in request body` }
+          })
+      })
+    })
+
+    it('removes XSS attack content from response', () => {
+      const { maliciousArticle, expectedArticle } = makeMaliciousArticle()
+      return supertest(app)
+        .post(`/articles`)
+        .send(maliciousArticle)
+        .expect(201)
+        .expect(res => {
+          expect(res.body.title).to.eql(expectedArticle.title)
+          expect(res.body.content).to.eql(expectedArticle.content)
+        })
     })
   })
 })
